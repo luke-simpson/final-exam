1f335f3129b17cb005b9ea530ff172a45932dbc9
Sara Howden <lhsimpson@madisoncollege.edu>
2014-05-05 21:24:44 -0500

    Initial commit for final exam.

---
+++ app/controllers/models_controller.rb
@@ -0,0 +1,20 @@
+class ModelsController < ApplicationController
+  def index
+    @models = Model.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 MileageTracker::Application.routes.draw do
+  get "models/index"
+  get "models/show"
+  get "models/new"
+  get "models/create"
+  get "models/edit"
+  get "models/update"
+  get "makes/index"
+  get "makes/show"
+  get "makes/new"
+  get "makes/create"
+  get "makes/edit"
+  get "makes/update"
   resources :vehicles
   resources :mileages, only: [:index, :new, :create]
 
