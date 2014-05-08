59b60735a14def3eef340cb5d97c6aa92837cc32
Sara Howden <lhsimpson@madisoncollege.edu>
2014-05-07 19:37:25 -0500

    Trying to get the forms going, needs work to initialize @make and @model -- currently nil.

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -1,14 +1,18 @@
 class MakesController < ApplicationController
   def index
+    @makes = Make.all
   end
 
   def show
   end
 
   def new
+    @make = Make.new
   end
 
   def create
+    @make = Make.new
+    # render 'new'
   end
 
   def edit
